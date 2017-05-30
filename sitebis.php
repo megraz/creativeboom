3d0784474b438f29e5671126196fb7f0a0eabfa6
Egraz Marlene <marlene.egraz@gmail.com>
2017-05-30 22:47:55 +0200

    modif sitebis

--- sitebis.php
+++ sitebis.php
@@ -6,15 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="css/style.css">
 
-    <title>Document</title>
+    <title>Creative Boom</title>
 </head>
 <body>
     <?php
     include('header.php');
     ?>
-     <?php
+
+    <!--lead article-->
+    <section class="banniere">
+        <a href="http://www.creativeboom.com/inspiration/a-creatives-guide-to-new-york-city/">
+        <h2>A creative's guide to New York City</h2>
+        <p>19th April in <a href="#">Inspiration / Graphic Design</a></p>
+        </a>
+    </section>
+    <?php
     include('inspiration.php');
     ?>
+
     <?php
     include('footer.php');
     ?>
